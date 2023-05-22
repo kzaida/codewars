9fc37da5a4124b48bf2c6974869750a5984aad4e
khalid <khalid.zaida@orange.com>
2023-05-22 09:26:01 +0200

    Solution

--- src/main/kotlin/TotalAmountOfPoints.kt
+++ src/main/kotlin/TotalAmountOfPoints.kt
@@ -1,7 +1,23 @@
 class TotalAmountOfPoints {
 
-    fun points(games: List<String>): Int {
+    fun points(matchesResult: List<String>): Int {
 
-        return 0
+        var team1FinalScore = 0
+        matchesResult.map {
+            var matchResult = it.split(":")
+            val scoreTeam1 = matchResult[0].toInt()
+            val scoreTeam2 = matchResult[1].toInt()
+            team1FinalScore += Match(scoreTeam1, scoreTeam2).getTeam1Points()
+        }
+
+        return team1FinalScore
+    }
+
+    data class Match(val scoreTeam1: Int, val scoreTeam2: Int) {
+        fun getTeam1Points(): Int {
+            if (scoreTeam1 > scoreTeam2) return 3
+            if (scoreTeam1 < scoreTeam2) return 0
+            return 1
+        }
     }
 }
